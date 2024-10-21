ce0b581900b3494198a00f60405a5899b7c9b4e2
Mike Drummond <mdrummond@etantolling.com>
2024-10-20 20:47:55 -0700

    adding asgn4

--- asgn4_cribbins.js
+++ asgn4_cribbins.js
@@ -2,8 +2,46 @@ var $ = function(id) {
     return document.getElementById(id)
 }
 var BeatlesArray;
-var displayMsg = function () {
+
+var displayMsg = function (event) {
+    var allNames = "";
+    for (item in BeatlesArray){
+        // Item number
+        itemNumber = parseInt(item) + 1
+
+        if (item == BeatlesArray.length - 1){
+            // Don't write comma
+            allNames += itemNumber + ". " + BeatlesArray[item.toString()];
+        } else {
+            
+            allNames += itemNumber + ". " + BeatlesArray[item.toString()] + ", ";
+        }    
+    }
+    $("list").innerHTML = allNames;
+    
 }
+function handleClick(event){
+    // Reset all borders
+    $("john").border = "0px";
+    $("paul").border = "0px";
+    $("george").border = "0px";
+    $("ringo").border = "0px";
+    // Set specific border
+    event.target.border = "4px";
+    event.target.style.color = "yellow";
+
+    BeatlesArray.push(event.target.id);
+
+}
+
 window.onload = function () {
-    $("john").onclick = alert();
+    BeatlesArray = new Array();
+    
+    $("john").onclick = handleClick;
+    $("paul").onclick = handleClick;
+    $("george").onclick = handleClick;
+    $("ringo").onclick = handleClick;
+
+    $("showlist").onclick = displayMsg;
 }
+
