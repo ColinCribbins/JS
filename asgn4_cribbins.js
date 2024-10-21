963908294269ba9c0bd1cdb39fbfaa7543e98b4b
Colin <cribbinscolin@gmail.com>
2024-10-21 01:58:04 +0000

    adding asgn4

---
+++ asgn4_cribbins.js
@@ -0,0 +1,9 @@
+var $ = function(id) {
+    return document.getElementById(id)
+}
+var BeatlesArray;
+var displayMsg = function () {
+}
+window.onload = function () {
+    $("john").onclick = alert();
+}
